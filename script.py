c0933b17b6efc7000e23ea481fc5ede8cc551be2
rand1nhof1-byte <rand1nhof1@gmail.com>
2025-10-23 10:20:57 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,6 @@
 import psycopg2
 
 # Database connection details (from docker-compose.yml)
-DB_HOST = "40.67.225.187"   # If running on same machine; use server IP if remote
-DB_PORT = "5432"
-DB_NAME = "discordbot"
-DB_USER = "rand1nho"
-DB_PASS = "rand1nho_admin"
 
 try:
     # Connect to PostgreSQL
@@ -33,3 +28,4 @@ try:
 
 except Exception as e:
     print("Error:", e)
+
